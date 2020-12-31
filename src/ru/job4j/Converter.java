4815e06d77d022cb051c8cb3ab022d5d8c7e9320
HEAVYxRAIN <heavyxrain@yandex.ru>
2020-12-31 09:35:01 +0300

    5. Конвертер валюты [#438252]

---
+++ src/ru/job4j/Converter.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
+        return rsl;
+    }
+
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60; /* формула перевода рублей в доллары. */
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(70);
+        System.out.println("70 rubles are " + euro + " euro.");
+        int dollar = Converter.rubleToDollar(60);
+        System.out.println("60 rubles are " + dollar + " dollar.");
+    }
+}
+
