263445c592ff49960afcaf92c5ba91a620d99dfa
HEAVYxRAIN <heavyxrain@yandex.ru>
2021-01-11 19:37:46 +0300

    4.1.2. Глупый бот [#438196]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -3,10 +3,9 @@ package ru.job4j.condition;
 public class DummyBot {
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
-        if ("Привет, Бот".equals(question)) {
+        if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-        } else if ("Как ты?".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
          return rsl;
@@ -14,9 +13,11 @@ public class DummyBot {
 
 
     public static void main(String[] args) {
-        String rsl = DummyBot.answer("Привет, Бот");
+        String rsl = DummyBot.answer("Привет, Бот.");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Это ставит меня в тупик. Задайте другой вопрос.");
         System.out.println(rsl);
     }
 }
