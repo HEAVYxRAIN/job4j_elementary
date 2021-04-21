0e9eb95389f70454a240ce413dad878e215d3930
HEAVYxRAIN <heavyxrain@yandex.ru>
2021-04-21 18:23:23 +0300

    6.4.2. Слово заканчивается на ... [#438226]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         for (int i = 0; i < post.length; i++) {
-            if (post[post.length - 1] == word[word.length - 1]) {
-                return true;
+            if (post[post.length - i - 1] != word[word.length - i - 1]) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
 
