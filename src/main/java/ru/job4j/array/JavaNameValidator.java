adc6e3795749c7f820d78757413f41a9e01c7246
HEAVYxRAIN <heavyxrain@yandex.ru>
2023-01-25 16:06:48 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean result = false;
+        if (!name.isEmpty() && !Character.isUpperCase(name.codePointAt(0))
+                && !Character.isDigit(name.codePointAt(0))) {
+            for (int i = 0; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (isLowerLatinLetter(code) || isSpecialSymbol(code) || isUpperLatinLetter(code)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
